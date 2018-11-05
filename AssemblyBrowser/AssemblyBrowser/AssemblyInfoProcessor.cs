ac58cc74e31ab44743b7ad8710e7b6d1604dd7e1
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-11-06 00:08:48 +0300

    added class for processing assembly info

---
+++ AssemblyBrowser/AssemblyBrowser/AssemblyInfoProcessor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    class AssemblyInfoProcessor
+    {
+        private Assembly asm;
+
+        public AssemblyInfoProcessor(Assembly _asm)
+        {
+            asm = _asm;
+        }
+
+        public Assembly Assembly
+        {
+            get
+            {
+                return asm;
+            }
+            private set { }
+        }
+
+        public AssemblyResult GetStructeredInfo()
+        {
+            List<Type> asmTypes = new List<Type>(GetLoadableTypes());
+            List<string> namespaces = new List<string>(asmTypes.Where(item => item.Namespace != null).Select(t => t.Namespace).Distinct());
+            List<NamespaceResult> namespacesResult = new List<NamespaceResult>();
+            foreach (string namesp in namespaces)
+            {
+                List<Type> namespaceTypes = new List<Type>(asmTypes.Where(t => t.Namespace == namesp && !t.Name.Contains("<>c") && !Regex.Match(t.Name, @"^[<]\w+[>]*", RegexOptions.IgnoreCase).Success));
+                List<TypeResult> typeResults = new List<TypeResult>();
+                foreach (Type type in namespaceTypes)
+                {
+                    TypeResult typeRes = new TypeResult(type);
+                    typeResults.Add(typeRes);
+                }
+                NamespaceResult namespRes = new NamespaceResult(namesp, new List<TypeResult>(typeResults));
+                namespacesResult.Add(namespRes);
+            }
+            return new AssemblyResult(new List<NamespaceResult>(namespacesResult));
+        }
+
+        private IEnumerable<Type> GetLoadableTypes()
+        {
+            if (Assembly == null) throw new ArgumentNullException(nameof(Assembly));
+            try
+            {
+                return Assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                return e.Types.Where(t => t != null);
+            }
+        }
+    }
+}
