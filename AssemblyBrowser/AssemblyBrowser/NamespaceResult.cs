bd17f13935abb21b8b40fbac0fa3dbfd905910a2
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-11-06 00:03:02 +0300

    added class for Assembly namespace

---
+++ AssemblyBrowser/AssemblyBrowser/NamespaceResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    class NamespaceResult
+    {
+        private string namespaceName;
+        private List<TypeResult> dataTypes;
+
+        public NamespaceResult(string name, List<TypeResult> types)
+        {
+            namespaceName = name;
+            dataTypes = new List<TypeResult>(types);
+        }
+
+        public string Name => namespaceName;
+
+        public List<TypeResult> DataTypeResult
+        {
+            get
+            {
+                return dataTypes;
+            }
+            set
+            {
+                dataTypes = new List<TypeResult>(value);
+            }
+        }
+    }
+}
