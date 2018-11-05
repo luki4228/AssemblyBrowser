44965960bd4b4e3692d0fa82fd9ce97177478c01
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-11-06 00:04:52 +0300

    added class for result structure

---
+++ AssemblyBrowser/AssemblyBrowser/AssemblyResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyBrowser
+{
+    class AssemblyResult
+    {
+        private List<NamespaceResult> namespaces;
+
+        public AssemblyResult(List<NamespaceResult> namespacesResults)
+        {
+            namespaces = namespacesResults;
+        }
+
+        public List<NamespaceResult> Namespaces
+        {
+            get { return namespaces; }
+            set { namespaces = new List<NamespaceResult>(value); }
+        }
+    }
+}
