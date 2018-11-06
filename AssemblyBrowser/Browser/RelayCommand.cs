471a59f8fee51470856288b463d13d391dc54fa3
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-11-06 12:30:30 +0300

    added class for commands

---
+++ AssemblyBrowser/Browser/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Browser
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private bool canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, bool canExecute = true)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
