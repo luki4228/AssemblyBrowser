9d7713e78a0a051ececb29fde342754a995321ed
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-11-13 21:01:20 +0300

    added class for OpenDialog

---
+++ AssemblyBrowser/Browser/Service.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser
+{
+    class Service
+    {
+        public string OpenFileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Assembly (*.dll) | *.dll";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                return openFileDialog.FileName;
+            }
+            return null;
+        }
+    }
+}
