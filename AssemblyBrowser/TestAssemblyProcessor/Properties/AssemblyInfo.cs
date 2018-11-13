c1e216bba93380148e823989160af52d01ab8296
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-11-13 22:14:23 +0300

    added Tests

---
+++ AssemblyBrowser/TestAssemblyProcessor/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestAssemblyProcessor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestAssemblyProcessor")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b74bb45-0539-44ee-aa36-11749382a746")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssemblyBrowser/TestAssemblyProcessor/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyBrowser;
+using System.Reflection;
+using System.Linq;
+
+namespace TestAssemblyProcessor
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private AssemblyResult asmResult;
+        public string NameTest { get; set; }
+        private Type testClassType;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Assembly asm = Assembly.ReflectionOnlyLoadFrom("./TestAssemblyProcessor.dll");
+            AssemblyInfoProcessor asmInfoProcessor = new AssemblyInfoProcessor(asm);
+            asmResult = asmInfoProcessor.GetStructeredInfo();
+            testClassType = typeof(UnitTest1);
+        }
+
+        [TestMethod]
+        public void TestNamespacesIsNotNull()
+        {
+            Assert.IsNotNull(asmResult.Namespaces);
+        }
+
+        [TestMethod]
+        public void TestNamespacesCount()
+        {
+            Assert.IsTrue(asmResult.Namespaces.Count > 0);
+        }
+
+        [TestMethod]
+        public void TestNamespaceName()
+        {
+            Assert.AreEqual(asmResult.Namespaces[0].Name, nameof(TestAssemblyProcessor));
+        }
+
+        [TestMethod]
+        public void TestNamespacesTypesCount()
+        {
+            foreach (NamespaceResult nmp in asmResult.Namespaces)
+            {
+                Assert.IsTrue(nmp.DataTypeResult.Count > 0);
+            }
+        }
+
+        [TestMethod]
+        public void TestTypeFieldsCount()
+        {
+            int actualCount = asmResult.Namespaces[0].DataTypeResult[0].Fields.Count;
+            int expectedCount = testClassType.GetFields().Length;
+            Assert.AreEqual(actualCount, expectedCount);
+        }
+
+        [TestMethod]
+        public void TestTypePropertiesCount()
+        {
+            int actualCount = asmResult.Namespaces[0].DataTypeResult[0].Properties.Count;
+            int expectedCount = testClassType.GetProperties().Length;
+
+            Assert.AreEqual(actualCount, expectedCount);
+        }
+
+        [TestMethod]
+        public void TestTypeMethodsCount()
+        {
+            int actualCount = asmResult.Namespaces[0].DataTypeResult[0].Methods.Count;
+            int expectedCount = testClassType.GetMethods().Where(item => !item.IsSpecialName).Count();
+            Assert.AreEqual(actualCount, expectedCount);
+        }
+    }
+}
